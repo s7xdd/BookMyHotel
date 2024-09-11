0c4150c1a8e1b94848687768d3419514178b6e3c
Mohammed <kmshamil123@gmail.com>
2024-09-12 00:51:17 +0530

    Added navbar

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import '../styles/banner.css'
+import Navbar from "./Navbar";
 
 const Banner = () => {
   return (
     <div className="banner">
-        
+        <Navbar/>
         <div className="banner_content">
             <h2>Go Near</h2>
             <h5>
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../styles/navbar.css'
+import Header from './Header'
+
+class Navbar extends React.Component {
+    listener = null;
+    state = {
+      nav: false,
+    };
+    componentDidMount() {
+      window.addEventListener("scroll", this.handleScroll);
+    }
+  
+    handleScroll = () => {
+      if (window.pageYOffset > 70) {
+        if (!this.state.nav) {
+          this.setState({ nav: true });
+        }
+      } else {
+        if (this.state.nav) {
+          this.setState({ nav: false });
+        }
+      }
+    };
+  
+    render() {
+      return (
+        <div>
+          <div className={`Nav ${this.state.nav && "Nav__black"}`}>
+            <Header />
+          </div>
+        </div>
+      );
+    }
+  }
+export default Navbar
